dad03b065f9e33c489eddce4273af1a739a6510e
Himanshu Atwal <hematwal.2003@gmail.com>
2024-05-18 11:54:43 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Component/Navbar'
+import Home from './Component/Home'
+import Footer from './Component/Footer'
+
+const App = () => {
+  return (
+    <>   
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    
+    </>
+  
+  )
+}
+
+export default App
